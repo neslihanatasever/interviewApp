7d8762c44fbf10fe67bea542a2dc8594d5891eb4
neslihanatasever <neslihanatasvr111@gmail.com>
2024-03-20 03:48:22 +0300

    [#2] giriş ekranı kart tasarımı

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
 export default function Home() {
   return (
-    <main className="mx-auto flex flex-col justify-center items-center text-center" style={{ height: "calc(100vh - 110px)" }}>
-      <div>
-        <h1>Mülakat Uygulamasına Hoş Geldiniz!</h1>
-        <p>Kendinizi test etmenin tam zamanı!</p>
+    <main className="mx-auto flex flex-col justify-center items-center text-center gap-9" style={{ height: "calc(100vh - 110px)" }}>
+      <div className="flex gap-5 flex-col">
+        <p className="font-bold text-4xl">Mülakat Uygulamasına Hoş Geldiniz!</p>
+        <p className="text-2xl">Kendinizi test etmenin tam zamanı!</p>
       </div>
 
 
-      <div className="techBox">
+      <div className="techBox flex gap-5">
         <div className="w-80 h-40 flex items-center justify-center flex-col gap-5 bg-[#EAE9E9] rounded-md">
-          <p>JavaScript</p>
-          <p>Mülakat Soruları</p>
+          <p>JavaScript Mülakat Soruları</p>
+        </div>
+        <div className="w-80 h-40 flex items-center justify-center flex-col gap-5 bg-[#EAE9E9] rounded-md">
+          <p>React Mülakat Soruları</p>
+        </div>
+        <div className="w-80 h-40 flex items-center justify-center flex-col gap-5 bg-[#EAE9E9] rounded-md">
+          <p>CSS Mülakat Soruları</p>
         </div>
       </div>
     </main>
