887381a264cc5f375361d96a5554c4b0521d613b
neslihanatasever <neslihanatasvr111@gmail.com>
2024-03-20 06:05:21 +0300

    [#3] seviye seçme ekranı

---
+++ app/level/page.tsx
@@ -0,0 +1,11 @@
+export default function LevelsPage() {
+    return(
+        <main className="mx-auto container w-full h-[100vh] flex flex-col justify-center items-center text-center gap-9">
+            <div>
+                <p>Mülakata girmek istediğiniz seviyeyi seçin</p>
+            </div>
+
+            
+        </main>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+import Link from "next/link"
+
 export default function Home() {
   return (
-    <main className="mx-auto flex flex-col justify-center items-center text-center gap-9" style={{ height: "calc(100vh - 110px)" }}>
+    <main className="mx-auto container w-full h-[100vh] flex flex-col justify-center items-center text-center gap-9" >
       <div className="flex gap-5 flex-col">
         <p className="font-bold text-4xl text-[#b6b5b5]">Mülakat Uygulamasına Hoş Geldiniz!</p>
         <p className="text-2xl text-[#C06014]">Kendinizi test etmenin tam zamanı!</p>
@@ -8,15 +10,15 @@ export default function Home() {
 
 
       <div className="techBox flex gap-5 ">
-        <div className="w-80 h-40 flex items-center justify-center flex-col gap-5 bg-[#b6b5b5] rounded-md shadow-xl shadow-[#000] hover:shadow-[#C06014]">
+        <Link className="w-80 h-40 flex items-center justify-center flex-col gap-5 bg-[#b6b5b5] rounded-md shadow-xl shadow-[#000] hover:shadow-[#C06014]" href={"/level"}>
           <p className="text-lg font-semibold">JavaScript Mülakat Soruları</p>  
-        </div>
-        <div className="w-80 h-40 flex items-center justify-center flex-col gap-5 bg-[#b6b5b5] rounded-md shadow-xl shadow-[#000] hover:shadow-[#C06014]">
+        </Link>
+        <Link className="w-80 h-40 flex items-center justify-center flex-col gap-5 bg-[#b6b5b5] rounded-md shadow-xl shadow-[#000] hover:shadow-[#C06014]" href={"/"}>
           <p className="text-lg font-semibold">React Mülakat Soruları</p>
-        </div>
-        <div className="w-80 h-40 flex items-center justify-center flex-col gap-5 bg-[#b6b5b5] rounded-md shadow-xl shadow-[#000] hover:shadow-[#C06014]">
+        </Link>
+        <Link className="w-80 h-40 flex items-center justify-center flex-col gap-5 bg-[#b6b5b5] rounded-md shadow-xl shadow-[#000] hover:shadow-[#C06014]" href={"/"}>
           <p className="text-lg font-semibold">CSS Mülakat Soruları</p>
-        </div>
+        </Link>
       </div>
     </main>
   );
