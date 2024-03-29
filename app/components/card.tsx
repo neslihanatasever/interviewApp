370ad7a80ecbbff9c5253743b6839f04a996b8b9
neslihanatasever <neslihanatasvr111@gmail.com>
2024-03-29 21:28:42 +0300

    [ #3]: level page ekranını tasarladık ve propslarını hazırladık

---
+++ app/components/card.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function Card(props:any) {
+    return (
+        <div className="my-5">
+            <Link href={props.url} className="flex justify-center items-center gap-5">
+                <div className="border-b border-t h-12 w-16 flex justify-center items-center text-2xl">{props.number}</div>
+                <div>
+                    <h3 className="text-xl">{props.head}</h3>
+                    <p className="opacity-50">{props.description}</p>
+                </div>
+            </Link>
+        </div>
+    )
+}
--- app/level/page.tsx
+++ app/level/page.tsx
@@ -1,8 +1,19 @@
 "use client";
-import { Box, Card, Flex, Inset, ScrollArea, Strong, Text } from "@radix-ui/themes";
+
+import Card from "../components/card";
 
 export default function LevelsPage() {
     return (
-        <div></div>
+        <div className="mx-auto container">
+            <div className="min-h-[100vh] flex justify-center items-center flex-col">
+            <h2 className="text-4xl">Hangi seviye teste girmek istiyorsunuz?</h2>
+            <p className="w-[50%] text-center my-5 opacity-50">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic perferendis esse aliquam veniam accusantium, eaque recusandae porro.</p>
+
+            <div>
+                <Card head="Temel seviye JavaScript Testi" url="bgfbgfs" number="1" description="Bu testte 15 soruluktur ve sizin temel js bilginizi ölçer. " />
+            </div>
+            </div>
+        </div>
+
     )
 }
